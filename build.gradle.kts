904208a4dea9582026ef78feb32268fc13520af5
Kengo TODA <skypencil@gmail.com>
2022-04-26 20:03:05 +0800

    build: re-introduce Gradle to build the project.

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+import jp.skypencil.flix.FlixCompile
+import jp.skypencil.flix.FlixTest
+plugins {
+    id("jp.skypencil.flix-base") version "1.1.1"
+}
+
+val compileFlix = tasks.register<FlixCompile>("compileFlix") {
+    source = fileTree("src")
+    destinationDirectory.set(file("build/classes/flix/main"))
+}
+val testFlix = tasks.register<FlixTest>("testFlix") {
+    source = fileTree("test")
+    destinationDirectory.set(file("build/classes/flix/test"))
+    report.set(file("build/reports/flix/main.txt"))
+}
+
+tasks.named<Task>("assemble") { dependsOn(compileFlix) }
+tasks.named<Task>("check") { dependsOn(testFlix) }
